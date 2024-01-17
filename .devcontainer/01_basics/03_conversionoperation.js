da33d2f7629c520731c2670704e683d52c558b27
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-17 18:44:44 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let value = "33"
+
+//console.log(typeof value);
+let sumvalue = Number(value)
+//console.log(typeof sumvalue);
+
+//33=>Number
+//"33"=>String
+//Null=>obj=>NaN
+
+let IslogIn = 0
+let BooleanIslogIn = Boolean(IslogIn)
+console.log(BooleanIslogIn);
+
+//1=>true
+//0=>false
+//""=>false
+//"vibhor"=>true
