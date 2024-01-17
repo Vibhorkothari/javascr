6437633217a1c3b1b9545f0e4807e74cba9fa306
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-17 20:00:02 +0000

    alldatatypes

---
+++ .devcontainer/01_basics/05_alldatatypes.js
@@ -0,0 +1,14 @@
+// non primitive datatypes:-
+
+// array, object, function
+//array
+const a = ["shaktiman", "thor", "captain america"];
+//object
+let myObj = {
+    name:Vibhor,
+    age : 24,
+}
+//function
+const Myfun = function(){
+    console.log("hello world");
+}
