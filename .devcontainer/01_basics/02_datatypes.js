ce5f8fa8f8c23991466a26aac97d626c83ce91c9
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-17 11:12:41 +0000

    javascript config datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer
+
+//alert(3 + 3)// only used in browser inspect not in nodejs
+
+console.log(3 +
+    3)// code readability should be high
+
+    /* datatypes are :-
+    Null
+    Numbers
+    String
+    Symbol
+    Boolean
+    BigInt
+    Undefined
+    */
+let name = "vibhor"
+
+    //object
+
+    console.log(typeof name);
+   console.log(typeof null);//object
