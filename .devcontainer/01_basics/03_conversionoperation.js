37d10810293e3ce53524cf919b5c461453a4652b
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-17 19:29:33 +0000

    conversion

--- .devcontainer/01_basics/03_conversionoperation.js
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -9,10 +9,34 @@ let sumvalue = Number(value)
 //Null=>obj=>NaN
 
 let IslogIn = 0
-let BooleanIslogIn = Boolean(IslogIn)
-console.log(BooleanIslogIn);
+//let BooleanIslogIn = Boolean(IslogIn)
+//console.log(BooleanIslogIn);
 
 //1=>true
 //0=>false
 //""=>false
 //"vibhor"=>true
+
+//      operations         //
+
+let a = 3
+let Anegative = -a
+console.log(Anegative);
+
+let str1 = 2
+let str2 = 3
+let str3 = str1 + str2
+console.log(str3);
+
+let str4 = "Vibhor"
+let str5 = " Kothari"
+console.log(str4 + str5);
+
+
+console.log(2 + 2);
+console.log("2" + 2);
+console.log(2 + "2");
+console.log("2" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
