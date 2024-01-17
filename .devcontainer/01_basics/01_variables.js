e0e16fe30045b1063ed9b8ca8a9de7a006672371
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-17 10:31:09 +0000

    javascript config variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountName = "vibhor"
+let accountEmail = "vibhorkothari4@gmail.com"
+var accountNo = "12345"
+accountDetails = "dehradun"
+
+//accountName = "kothari"
+
+/*
+dont use var only use let and const for variables
+*/
+accountNo = "67890"
+accountEmail = "kotharivibhor@gmail.com"
+console.log(accountEmail);
