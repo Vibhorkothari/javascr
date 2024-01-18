d2bbc0725e10dd554c4cd028ece9f838c4aeb2ae
Vibhorkothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-18 08:22:35 +0000

    datatypes

--- .devcontainer/01_basics/05_alldatatypes.js
+++ .devcontainer/01_basics/05_alldatatypes.js
@@ -2,13 +2,42 @@
 
 // array, object, function
 //array
-const a = ["shaktiman", "thor", "captain america"];
+//const a = ["shaktiman", "thor", "captain america"];
 //object
-let myObj = {
-    name:Vibhor,
-    age : 24,
-}
+//let myObj = {
+   // name:Vibhor,
+   // age : 24,
+//}
 //function
-const Myfun = function(){
-    console.log("hello world");
+//const Myfun = function(){
+    //console.log("hello world");
+//}
+
+
+
+// two types of memeories:-
+// (Stack), (Heap)
+
+//Stack->primitive datatypes (copies the variable value)
+//Heap->Non-Primitive dataypes (Reference of the value)
+
+
+//Stack memory
+let user = "vibhor"
+let user1 = user
+
+user1 = "kothari"
+
+console.log(user);
+console.log(user1);
+
+//Heap memory
+let userone = {
+    email:"vibhorkothari@gmail.com",
+    id:"1234",
 }
+let usertwo = userone
+usertwo.email = "kotharivibhor@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
