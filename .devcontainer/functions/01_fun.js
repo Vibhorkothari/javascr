7fea6293a4589ddac2cab0ac28e4be10fd4bf4d3
Vibhor Kothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-25 12:16:47 +0000

    function all

--- .devcontainer/functions/01_fun.js
+++ .devcontainer/functions/01_fun.js
@@ -23,7 +23,7 @@ function addtwonumbers(number1 , number2){
     return result                   // here comes a major thing that if we write console.log after return statement then it is not seen or call because return function already tells that here the function ends so console.log value will only called before return statement
 }
 const result = addtwonumbers(3 , 9)
-console.log("Result", result);
+//console.log("Result", result);
 
 
 function loginuser(username){
@@ -34,4 +34,31 @@ function loginuser(username){
     }
     return ` ${username} just logged in`
 }
-console.log(loginuser());
+//console.log(loginuser());
+
+
+//if parameters are large in number then we use rest operator same as spread operator(syntax= ...parametername)
+function giveparameter(...num1){
+return num1
+}
+console.log(giveparameter(200,300,400,20000));
+
+//object data convert to function
+const user = {
+    name:"vibhor",
+    mail:"kothari@gmail.com",
+}
+function latest(user2){
+    console.log(`name of user is ${user2.name} and mail id is ${user2.mail}`);
+}
+latest(user)
+
+//array to function
+const myarray = [600,700,800]
+
+function mynewarray(array0){
+return array0[1]
+}
+console.log(mynewarray(myarray));
+
+
