ab1b99f55762cb049a42d32820f3bb786036fa53
Vibhor Kothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-26 18:33:04 +0000

    ec

---
+++ .devcontainer/javascript execution context/ec.js
@@ -0,0 +1,14 @@
+//execution context means how javascript will run or execute the file
+//     interview question
+//browsers global ec is different from others global ec because browsers global ec is important and in this (THIS) value comes as window object
+//javascript is single threaded
+
+
+// There are three types of execution context that are:-
+// 1.global execution context
+// 2.function execution context
+// 3.eval execution context
+
+//there are two types of phase:-
+// 1.memory creation phase
+// 2.execution phase
