966f04de5af6a138c3d0517e35f59d15b449f709
Vibhor Kothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-27 11:14:17 +0000

    all about if else

---
+++ .devcontainer/control_flow/01_control.js
@@ -0,0 +1,18 @@
+//          control statements
+//         == , === , ! ,<,>,<=,>=,!==,!=
+const num1=10
+if(num1 ==="10"){
+    const ram = "ten"
+    console.log(`number is : ${ram}`);
+}
+else{
+    const ram1 = "not ten"
+    console.log(`number is: ${ram1}`);
+}
+
+
+// Another way of writing if else condition is 
+
+const sum =1000
+if(sum > 500) console.log(`it is greater`);
+
