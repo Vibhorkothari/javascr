9c549ccee6a3f2ee8f5301dec2f944b66a4ac251
Vibhor Kothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-28 17:46:08 +0000

    dowhile loop

---
+++ .devcontainer/06_loops/dowhile.js
@@ -0,0 +1,23 @@
+//while loop
+let i =1
+while (i <10) {
+//console.log(`value is ${i}`);
+i = i*2    
+}
+
+const myarray=["vibhor","harshita","aman"]
+let array= 0
+while (array < myarray.length) {
+   // console.log(`value is ${myarray[array]}`);
+    array =array+1
+    
+}
+
+
+// Do while loop
+
+let start =2
+do {
+    console.log(`score is ${start}`);
+    start =start*2
+} while (start<=10);
