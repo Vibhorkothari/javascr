41e58e5f84107d97f769dec9d48c32285bb17cfe
Vibhor Kothari <86224604+Vibhorkothari@users.noreply.github.com>
2024-01-28 07:51:25 +0000

    if else

--- .devcontainer/control_flow/01_control.js
+++ .devcontainer/control_flow/01_control.js
@@ -1,18 +1,34 @@
 //          control statements
-//         == , === , ! ,<,>,<=,>=,!==,!=
+//         == , === , ! ,<,>,<=,>=,!==,!=,||
 const num1=10
 if(num1 ==="10"){
     const ram = "ten"
-    console.log(`number is : ${ram}`);
+    //console.log(`number is : ${ram}`);
 }
 else{
     const ram1 = "not ten"
-    console.log(`number is: ${ram1}`);
+    //console.log(`number is: ${ram1}`);
 }
 
 
 // Another way of writing if else condition is 
 
 const sum =1000
-if(sum > 500) console.log(`it is greater`);
+//if(sum > 500) console.log(`it is greater`);
+
+const usernameLogin= true
+const passwordLogin = false
+if(usernameLogin && passwordLogin)
+{
+    console.log(`Allow to login on Instagram`);
+}
+else if(usernameLogin == false) {
+    console.log(`username is incorrect`);
+}
+else
+{
+    console.log(`password is incorrect`);
+}
+
+
 
